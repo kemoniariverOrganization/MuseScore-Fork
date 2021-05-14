386975f4f48cb70e76255aa9dbf86e4c8c144c0e
andreoid87 <accordinoandrea12@gmail.com>
2021-05-14 17:31:05 +0200

    Aggiornato script mac per compilazione e package

---
+++ deployMac.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export PATH=".:${PATH}:${HOME}/Qt/5.15.2/clang_64/bin"
+make -f Makefile.osx revision -j 12
+make -f Makefile.osx release -j 12
+make -f Makefile.osx install -j 12
+make -f Makefile.osx package -j 12
