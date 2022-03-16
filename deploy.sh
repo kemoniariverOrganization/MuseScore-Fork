e394eaaaaaee3c1197c1dc0b680ced1ada975daa
MacMini <>
2022-03-16 21:57:48 +0100

    Migliorato script deploy mac

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 export PATH=".:${PATH}:${HOME}/Qt/5.15.2/clang_64/bin"
+
+# Copy in installer folder
+cp -R ./applebuild/Musescore\ ODLA.app ../ODLA/installer-mac/packages/it.kemoniariver.musescore/data
+
+# Launch deploy script for Mac
 deployPath=$(which macdeployqt)
 qmlDir="${deployPath%macdeployqt}"../qml
-macdeployqt ./applebuild/Musescore\ ODLA.app -qmldir=$qmlDir
+macdeployqt ../ODLA/installer-mac/packages/it.kemoniariver.musescore/data/Musescore\ ODLA.app -qmldir=$qmlDir
+
